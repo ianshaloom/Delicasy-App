fea7fe4d625b231decdd1f883d1dcc8531ab4e11
ianshaloom <ianshaloom0@outlook.com>
2023-07-21 23:21:06 +0300

    new theme data and route

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,40 @@
-import 'package:deliapp/pages/homepage/homepage.dart';
 import 'package:flutter/material.dart';
+
+import 'utils/routes.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      title: 'Flutter Demo',
+      title: 'DeliApp',
       theme: ThemeData(
         primarySwatch: Colors.lightGreen,
         useMaterial3: true,
+        
+        // App Bar
         appBarTheme: const AppBarTheme(
           color: Colors.green,
-          toolbarHeight: 10,
+          foregroundColor: Colors.white,
+          titleTextStyle: TextStyle(
+            fontFamily: 'Montserrat',
+            fontWeight: FontWeight.w400,
+            fontSize: 22,
+          ),
+          centerTitle: true,
+        ),
+        
+        // Scaffold
+        scaffoldBackgroundColor: Colors.green,
+        
+        // Text
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(
+            fontFamily: 'Montserrat',
+          fontWeight: FontWeight.w300,
+          fontSize: 20,
+          )
         ),
       ),
-      home: const HomePage(),
+      routes: routes,
       debugShowCheckedModeBanner: false,
     ),
   );
